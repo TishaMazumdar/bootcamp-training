24e55a851ee40aa9abe5549bfbe0ca99d874a0d4
Tisha Mazumdar <tishamazumdar2004@gmail.com>
2025-09-25 09:37:18 +0530

    Escalator Program: Ratio Method

--- Escalator.java
+++ Escalator.java
@@ -15,12 +15,19 @@ public class Escalator{
         System.out.print("Enter time taken while walking: ");
         int w_time = sc.nextInt();
 
-        float step_diff = Math.abs(r_steps - w_steps);
-        float time_diff = Math.abs(r_time - w_time);
-        float time_per_step = step_diff / time_diff;
+        // float step_diff = Math.abs(r_steps - w_steps);
+        // float time_diff = Math.abs(r_time - w_time);
+        // float time_per_step = step_diff / time_diff;
 
-        float steps = r_steps + (r_time * time_per_step);
+        // float steps = r_steps + (r_time * time_per_step);
 
-        System.out.println("Number of steps: "+steps);
+        // System.out.println("Number of steps by linear function: "+steps);
+
+        // Using ratio
+        float N;
+        float time_ratio = (float)r_time / (float)w_time;
+        System.out.println("Time ratio: "+time_ratio);
+        N = (r_steps - (w_steps*time_ratio)) / (1-time_ratio);
+        System.out.println("Number of steps by ratio method: "+N);
     }
 }
